3958aec049f32b7b5cfb97444abc1ff218f5cc0e
Alexander Poslushnyak <fr4nk.horr1gan@gmail.com>
2021-01-19 00:29:34 +0200

    Added acf repeater field

--- wp-content/themes/astra/inc/acf-builder/acf-builder.php
+++ wp-content/themes/astra/inc/acf-builder/acf-builder.php
@@ -50,25 +50,48 @@ final class ACF_Builder
         if (function_exists('acf_add_local_field')):
 
             acf_add_local_field(array(
-                'key' => 'field_repeater_builder',
-                'label' => esc_html__('Landing Page Builder', 'coach-core'),
-                'name' => 'option_repeater_builder',
-                'type' => 'repeater',
-                'instructions' => esc_html__('Build your own landing page', 'coach-core'),
+                'key' => 'field_repeater_builder_toggle',
+                'label' => esc_html__('Enable/Disable Landing Page Builder', 'neochrome'),
+                'name' => 'option_repeater_builder_toggle',
+                'type' => 'true_false',
                 'required' => 0,
                 'conditional_logic' => 0,
+                'default_value' => 0,
+                'ui' => 1,
+                'ui_on_text' => esc_html__('Enable', 'neochrome'),
+                'ui_off_text' => esc_html__('Disable', 'neochrome'),
+                'parent' => 'group_repeater_builder_settings',
+            ));
+
+            acf_add_local_field(array(
+                'key' => 'field_repeater_builder',
+                'label' => esc_html__('Landing Page Builder', 'neochrome'),
+                'name' => 'option_repeater_builder',
+                'type' => 'repeater',
+                'label_placement' => 'top',
+                'instructions' => esc_html__('Build your own landing page', 'neochrome'),
+                'required' => 0,
                 'min' => 0,
                 'max' => 0,
                 'layout' => 'block',
-                'button_label' => esc_html__('Add new Section', 'coach-core'),
+                'button_label' => esc_html__('Add new Section', 'neochrome'),
                 'parent' => 'group_repeater_builder_settings',
+                'conditional_logic' => array(
+                    array(
+                        array(
+                            'field' => 'field_repeater_builder_toggle',
+                            'operator' => '==',
+                            'value' => '1',
+                        ),
+                    ),
+                ),
                 'sub_fields' => array(
                     array(
                         'key' => 'field_builder_section_type',
-                        'label' => esc_html__('Section Type', 'coach-core'),
+                        'label' => esc_html__('Section Type', 'neochrome'),
                         'name' => 'option_builder_section_type',
                         'type' => 'select',
-                        'instructions' => esc_html__('Select section type', 'coach-core'),
+                        'instructions' => esc_html__('Select section type', 'neochrome'),
                         'required' => 0,
                         'conditional_logic' => 0,
                         'choices' => array(
